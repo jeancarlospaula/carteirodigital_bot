e6e61fa7ce1b648c69c39296c1a5e050d749cb68
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-09-17 15:08:39 -0300

    hotfix: Changed property name

--- src/models/trackedOrdersNumber.ts
+++ src/models/trackedOrdersNumber.ts
@@ -1,7 +1,7 @@
 import { Schema, model, Document } from 'mongoose'
 
 export interface ITrackedOrdersNumber extends Document {
-  number: number
+  trackedOrdersNumber: number
 }
 
 const trackedOrdersNumber = new Schema(
