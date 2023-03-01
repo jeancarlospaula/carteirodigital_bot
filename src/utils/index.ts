dabd0f629277d8dcb2bbe618434516446437816a
jean.depaula <jean.depaula@obraprimaweb.com.br>
2023-03-01 19:48:28 -0300

    chore: Add canSendDefaultMessage funtion import and export on index file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
+import { canSendDefaultMessage } from './canSendDefaultMessage'
 import { checkDeliveryOrder } from './checkDeliveryOrder'
 import { checkImportationOrder } from './checkImportationOrder'
 import { checkOrderPosted } from './checkOrderPosted'
 import { checkUpdateOrder } from './checkUpdateOrder'
 
 export {
+  canSendDefaultMessage,
   checkDeliveryOrder,
   checkImportationOrder,
   checkOrderPosted,
