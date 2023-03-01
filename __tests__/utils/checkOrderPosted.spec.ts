a49e7c079539fba7826ad9aecb63ac1edd4c369e
jean.depaula <jean.depaula@obraprimaweb.com.br>
2023-03-01 19:30:44 -0300

    test: Added test to checkOrderPosted function

---
+++ __tests__/utils/checkOrderPosted.spec.ts
@@ -0,0 +1,20 @@
+import { IEventMessage } from '../../src/lib/getLastUpdateMessage'
+import { checkOrderPosted } from '../../src/utils/checkOrderPosted'
+
+describe('checkOrderPosted', () => {
+  it('should return true if status contains "Objeto postado"', () => {
+    const event = {
+      status: 'Objeto postado',
+    }
+
+    expect(checkOrderPosted(event as IEventMessage)).toBeTruthy()
+  })
+
+  it('should return false if status does not contain "Objeto postado"', () => {
+    const event = {
+      status: 'Objeto saiu para entrega ao destinatário',
+    }
+
+    expect(checkOrderPosted(event as IEventMessage)).toBeFalsy()
+  })
+})
